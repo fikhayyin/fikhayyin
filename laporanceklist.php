2e576d49ec7a9d32de44c6e32dfdb61e28be5cef
fikhayyin <72871648+fikhayyin@users.noreply.github.com>
2020-10-19 20:29:08 +0700

    Update laporanceklist.php

--- laporanceklist.php
+++ laporanceklist.php
@@ -7,10 +7,10 @@ if (empty($_SESSION['admin'])) {
 } else {
 
     if ($_SESSION['admin'] != 1 and $_SESSION['admin'] != 3) {
-        echo '<script language="javascript">
+        <?= '<script language="javascript">
                     window.alert("ERROR! Anda tidak memiliki hak akses untuk membuka halaman ini");
                     window.location.href="./logout.php";
-                  </script>';
+                  </script>' ?>;
     } else {
 
         if (isset($_REQUEST['act'])) {
@@ -76,7 +76,7 @@ if (empty($_SESSION['admin'])) {
             <?php
             if (isset($_SESSION['succAdd'])) {
                 $succAdd = $_SESSION['succAdd'];
-                echo '<div id="alert-message" class="row">
+                <?= '<div id="alert-message" class="row">
                                 <div class="col m12">
                                     <div class="card green lighten-5">
                                         <div class="card-content notif">
@@ -84,12 +84,12 @@ if (empty($_SESSION['admin'])) {
                                         </div>
                                     </div>
                                 </div>
-                            </div>';
+                            </div>' ?>;
                 unset($_SESSION['succAdd']);
             }
             if (isset($_SESSION['succEdit'])) {
                 $succEdit = $_SESSION['succEdit'];
-                echo '<div id="alert-message" class="row">
+                <?= '<div id="alert-message" class="row">
                                 <div class="col m12">
                                     <div class="card green lighten-5">
                                         <div class="card-content notif">
@@ -97,12 +97,12 @@ if (empty($_SESSION['admin'])) {
                                         </div>
                                     </div>
                                 </div>
-                            </div>';
+                            </div>' ?>;
                 unset($_SESSION['succEdit']);
             }
             if (isset($_SESSION['succDel'])) {
                 $succDel = $_SESSION['succDel'];
-                echo '<div id="alert-message" class="row">
+                <?= '<div id="alert-message" class="row">
                                 <div class="col m12">
                                     <div class="card green lighten-5">
                                         <div class="card-content notif">
@@ -110,7 +110,7 @@ if (empty($_SESSION['admin'])) {
                                         </div>
                                     </div>
                                 </div>
-                            </div>';
+                            </div>' ?>;
                 unset($_SESSION['succDel']);
             }
             ?>
@@ -121,7 +121,7 @@ if (empty($_SESSION['admin'])) {
     <?php
             if (isset($_REQUEST['submit'])) {
                 $cari = mysqli_real_escape_string($config, $_REQUEST['cari']);
-                echo '
+                <?= '
                         <div class="col s12" style="margin-top: -18px;">
                             <div class="card blue lighten-5">
                                 <div class="card-content">
@@ -141,49 +141,49 @@ if (empty($_SESSION['admin'])) {
                                         <th width="16%">Tindakan <span class="right"><i class="material-icons" style="color: #333;">settings</i></span></th>
                                     </tr>
                                 </thead>
-                                <tbody>';
+                                <tbody>' ?>;
 
                 //script untuk mencari data
                 $query = mysqli_query($config, "SELECT * FROM checklist WHERE nama_cek LIKE '%$cari%' ORDER by id_cek DESC LIMIT $curr, 15");
                 if (mysqli_num_rows($query) > 0) {
                     $no = 1;
                     while ($row = mysqli_fetch_array($query)) {
-                        echo '
+                        <?= '
                                       <tr>
                                         <td>' . $row['no_cek'] . '<br/><hr/>' . $row['tipe'] . '</td>
-                                        <td>' . substr($row['nama_cek'], 0, 200) . '<br/><br/><strong></strong>';
+                                        <td>' . substr($row['nama_cek'], 0, 200) . '<br/><br/><strong></strong>' ?>;
 
                         if (!empty($row['file'])) {
-                            echo ' <strong><a href="?page=dlc&act=fcl&id_cek=' . $row['id_cek'] . '">' . $row['file'] . '</a></strong>';
+                            <?= ' <strong><a href="?page=dlc&act=fcl&id_cek=' . $row['id_cek'] . '">' . $row['file'] . '</a></strong>' ?>;
                         } else {
-                            echo ' <em>Tidak ada file yang diupload</em>';
+                            <?= ' <em>Tidak ada file yang diupload</em>' ?>;
                         }
-                        echo '</td>
+                        <?= '</td>
                                         <td>' . $row['hasil'] . '</td><td>' . $row['area'] . '<br/><hr/>' . indoDate($row['tgl_lpr']) . '</td>
-                                        <td>';
+                                        <td>' ?>;
 
                         if ($_SESSION['id_user'] != $row['id_user'] and $_SESSION['id_user'] != 1 and $_SESSION['id_user'] != 2) {
-                            echo '<button class="btn small blue-grey waves-effect waves-light"><i class="material-icons">error</i> No Action</button>';
+                            <?= '<button class="btn small blue-grey waves-effect waves-light"><i class="material-icons">error</i> No Action</button>' ?>;
                         } else {
-                            echo '<a class="btn small blue waves-effect waves-light" href="?page=ilc&act=edit&id_cek=' . $row['id_cek'] . '">
+                            <?= '<a class="btn small blue waves-effect waves-light" href="?page=ilc&act=edit&id_cek=' . $row['id_cek'] . '">
                                                     <i class="material-icons">edit</i> EDIT</a>
                                                 <a class="btn small deep-orange waves-effect waves-light" href="?page=ilc&act=del&id_cek=' . $row['id_cek'] . '">
-                                                    <i class="material-icons">delete</i> DEL</a>';
+                                                    <i class="material-icons">delete</i> DEL</a>' ?>;
                         }
-                        echo '
+                        <?= '
                                         </td>
-                                    </tr>';
+                                    </tr>' ?>;
                     }
                 } else {
-                    echo '<tr><td colspan="5"><center><p class="add">Tidak ada data yang ditemukan</p></center></td></tr>';
+                    <?= '<tr><td colspan="5"><center><p class="add">Tidak ada data yang ditemukan</p></center></td></tr>' ?>;
                 }
-                echo '</tbody></table><br/><br/>
+                <?= '</tbody></table><br/><br/>
                             </div>
                         </div>
-                        <!-- Row form END -->';
+                        <!-- Row form END -->' ?>;
             } else {
 
-                echo '
+                <?= '
                         <div class="col m12" id="colres">
                         <table class="bordered" id="tbl">
                             <thead class="blue lighten-4" id="head">
@@ -199,10 +199,10 @@ if (empty($_SESSION['admin'])) {
 
                                         <div id="modal" class="modal">
                                             <div class="modal-content white">
-                                                <h5>Jumlah data yang ditampilkan per halaman</h5>';
+//                                                 <h5>Jumlah data yang ditampilkan per halaman</h5>' ?>;
                 $query = mysqli_query($config, "SELECT id_atur,checklist FROM pengaturan");
                 list($id_atur, $checklist) = mysqli_fetch_array($query);
-                echo '
+                <?= '
                                                 <div class="row">
                                                     <form method="post" action="">
                                                         <div class="input-field col s12">
@@ -221,7 +221,7 @@ if (empty($_SESSION['admin'])) {
                                                                 </select>
                                                             </div>
                                                             <div class="modal-footer white">
-                                                                <button type="submit" class="modal-action waves-effect waves-green btn-flat" name="simpan">Simpan</button>';
+                                                                <button type="submit" class="modal-action waves-effect waves-green btn-flat" name="simpan">Simpan</button>' ?>;
                 if (isset($_REQUEST['simpan'])) {
                     $id_atur = "1";
                     $checklist = $_REQUEST['checklist'];
@@ -233,7 +233,7 @@ if (empty($_SESSION['admin'])) {
                         die();
                     }
                 }
-                echo '
+                <?= '
                                                                 <a href="#!" class=" modal-action modal-close waves-effect waves-green btn-flat">Batal</a>
                                                             </div>
                                                         </div>
@@ -245,7 +245,7 @@ if (empty($_SESSION['admin'])) {
                                 </tr>
                             </thead>
 
-                            <tbody>';
+                            <tbody>' ?>;
 
                 //script untuk mencari data
                 $query = mysqli_query($config, "SELECT * FROM checklist ORDER by id_cek DESC LIMIT $curr, $limit");
@@ -253,19 +253,19 @@ if (empty($_SESSION['admin'])) {
                     $no = 1;
                     $no_ceklist = 1;
                     while ($row = mysqli_fetch_array($query)) {
-                        echo '
+                        <?= '
                                   <tr>
                                     <td>' . $no_ceklist . '</td>
                                     <td>' . indoDate($row['tgl_lpr']) . '</td>
                                     <td>' . $row['tipe'] . '</td>
                                     <td>' . $row['area'] . '</td>
                                     <td>' . substr($row['nama_cek'], 0, 200) . '</td>
-                                    <td> <strong></strong>';
+                                    <td> <strong></strong>' ?>;
 
                         if (!empty($row['file'])) {
-                            echo ' <strong><a href="?page=dlc&act=fcl&id_cek=' . $row['id_cek'] . '">' . $row['file'] . '</a></strong>';
+                            <?= ' <strong><a href="?page=dlc&act=fcl&id_cek=' . $row['id_cek'] . '">' . $row['file'] . '</a></strong>' ?>;
                         } else {
-                            echo ' <em>Tidak ada file yang diupload</em>';
+                            <?= ' <em>Tidak ada file yang diupload</em>' ?>;
                         }
                         echo '</td>
                                     <td>' . $row['hasil'] . '</td>
